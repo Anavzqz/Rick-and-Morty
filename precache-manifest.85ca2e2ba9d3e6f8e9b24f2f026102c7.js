3bd81c7441aa9981422a6dfe306519ddcad8ec05
Ana Vázquez <ana.maria.vl@hotmail.com>
2019-03-03 23:38:44 -0600

    Updates

---
+++ precache-manifest.85ca2e2ba9d3e6f8e9b24f2f026102c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5838542392d79ddfb1f6",
+    "url": "/Rick-and-Morty/static/js/runtime~main.58385423.js"
+  },
+  {
+    "revision": "22ac64983f07fd9278a7",
+    "url": "/Rick-and-Morty/static/js/main.22ac6498.chunk.js"
+  },
+  {
+    "revision": "9b0b67b1a83180ae9751",
+    "url": "/Rick-and-Morty/static/js/2.9b0b67b1.chunk.js"
+  },
+  {
+    "revision": "22ac64983f07fd9278a7",
+    "url": "/Rick-and-Morty/static/css/main.584592c9.chunk.css"
+  },
+  {
+    "revision": "e6c01c4540197985ef5b2260e2126f04",
+    "url": "/Rick-and-Morty/index.html"
+  }
+];
