563848dd9ff1b433a8904cee54ebebdd4a450ae6
Ana Vázquez <ana.maria.vl@hotmail.com>
2019-03-04 00:03:29 -0600

    Se elimina componente Title por errores

--- src/Routes.js
+++ src/Routes.js
@@ -3,14 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 //import Characters from './components/Characters';
 import Details from './components/Details';
 import CharactersList from './components/CharacterList';
-import Title from './components/Title';
 
 const Routes = () => {
     return(
         <Switch>
             <Route exact path = '/' component = { CharactersList } />
             <Route path = '/details' component = { Details } />
-            <Route path = '/title' component = { Title } />
         </Switch>
     )
 }
